76c6f4aa2554dc02335c68f4a60a84b851ec0aaa
Miles Steele <miles@milessteele.com>
2012-08-22 23:15:42 -0400

    init

---
+++ common.py
@@ -0,0 +1,64 @@
+# python 2.7
+
+import random, itertools, pprint
+pp = pprint.pprint
+
+class Card(object):
+  def __init__(s, val):
+    s._val = val
+
+  def n(s):
+    return s._val
+
+  def __repr__(s):
+    return str(s._val)
+
+class StressfulAlgorithm(object):
+  # table : array of 4 Cards
+  # hands : array of 6 arrays of 4 Cards
+  # returns : None for no action, tuple of 2 Cards for a trade
+  def turn(s, table, hands):
+    return None
+
+def hands_are_solved(hands):
+  return all(map(lambda hand: all(map(lambda card: card.n == hand[0].n(), hand)), hands))
+
+def execute_trade(trade_cards, table, hands):
+  if trade_cards == None: return
+  c1, c2 = trade_cards
+  if c2 in table:
+    c1, c2 = c2, c1
+  table[table.index(c1)] = c2
+  for hand in hands:
+    if hand.count(c2) == 1:
+      hand[hand.index(c2)] = c1
+
+def main():
+  deck = reduce(lambda x,y:x+y, [[Card(n) for n in range(1,14)] for suit in xrange(4)], [])
+  random.shuffle(deck)
+
+  def group_n(a, n):
+    return map(lambda e: map(lambda t: t[0], e[1]), itertools.groupby(zip(a, [i / n for i in range(len(a))]), lambda e: e[1]))
+
+  table = deck[0:4]
+  hands1 = group_n(deck[4:28], 4)
+  hands2 = group_n(deck[28:52], 4)
+
+  algo1 = StressfulAlgorithm()
+  algo2 = StressfulAlgorithm()
+
+  while not all(map(lambda hands: hands_are_solved(hands), [hands1, hands2])):
+    print "\nturn\n"
+    print "table"
+    pp(table)
+
+    print "hands1"
+    pp(hands1)
+
+    print "hands2"
+    pp(hands2)
+
+    execute_trade(algo1.turn(table, hands1), table, hands1)
+    execute_trade(algo1.turn(table, hands2), table, hands2)
+
+main()
