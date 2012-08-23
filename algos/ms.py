800660bfeb0865dd39f4d782583db1cb9c054a0c
Miles Steele <miles@milessteele.com>
2012-08-23 00:34:18 -0400

    restructure

--- algos/ms.py
+++ algos/ms.py
@@ -1,19 +1,4 @@
-import random, itertools, pprint
-pp = pprint.pprint
-
-class StressfulAlgorithm(object):
-  # table : array of 4 Cards
-  # hands : array of 6 arrays of 4 Cards
-  # returns : None for no action, tuple of 2 Cards for a trade
-  def turn(s, table, hands):
-    return None
-
-class DoNothing(StressfulAlgorithm):
-  pass
-
-class TradeConstant(StressfulAlgorithm):
-  def turn(s, table, hands):
-    return table[0], hands[0][0]
+from common import *
 
 class MSSimple1(StressfulAlgorithm):
   def turn(s, table, hands):
